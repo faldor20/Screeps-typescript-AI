f4f84919b52f3c314dd243d4b4c55415fafc7023
Eli Dowling <eli.jambu@yahoo.com>
2018-06-01 22:20:43 +1000

    commented a thing

--- src/memory.d.ts
+++ src/memory.d.ts
@@ -4,10 +4,10 @@ interface CreepMemory {
     }
     role: string
     [name: string]: any
-    type: {
+/*     type: {
         [key: string]
         role:string
-    }
+    } */
 }
 interface FlagMemory { [name: string]: any }
 interface SpawnMemory { [name: string]: any }
